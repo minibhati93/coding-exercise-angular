f177be9407927aa741d3e0f08bcf33e26c443d41
Mini Bhati <mini.bhati@mheducation.com>
2021-05-20 23:26:52 +0530

    Added specs in Header component

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,11 +1,16 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { of } from 'rxjs/internal/observable/of';
+import { mockData } from '../mocks/mock-data';
+import { WeatherService } from '../service/weather-service/weather.service';
 
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let weatherService: WeatherService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -13,6 +18,7 @@ describe('HeaderComponent', () => {
       imports: [ HttpClientTestingModule ]
     })
       .compileComponents();
+      weatherService = TestBed.inject(WeatherService);
   }));
 
   beforeEach(() => {
@@ -23,5 +29,21 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display city name in the header', fakeAsync(() => {
+    const cityName = mockData.weather.list[0].name;
+    spyOn(weatherService, 'getCityName').and.returnValue(of(cityName));
+    component.ngOnInit();
+    tick();
+    fixture.detectChanges();
+    expect(component.city).toEqual(cityName);
+    const header = fixture.debugElement.query(By.css('nav span')).nativeElement;
+    expect(header.textContent).toEqual(cityName);
+  }));
+
+  it('should not display city name in the header', () => {
+    const header = fixture.debugElement.query(By.css('nav span'));
+    expect(header).toBeNull();
   });
 });
